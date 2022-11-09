2a6b98082b19bad7da4f2754fec4edc364df79f7
Matthew Aileru <m.a.aileru@gmail.com>
2022-11-09 06:12:25 +0000

    Re-upload of original folder
    Re-upload of original folder as I accidentally updated it previously with the "attempt 2" version. For now, I want to keep the 2 versions separate.

---
+++ src/components/Headline.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Headline() {
+    return (
+        <div className="headline">
+            <h1>Matthew Aileru</h1>
+            <h4>Frontend Developer</h4>
+            <a href="#">matthewaileru.website</a>
+        </div>
+    )
+}
