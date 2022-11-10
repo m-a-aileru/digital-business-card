23d7c888b6b7da9f69dd7b5247a500a3a4070d79
Matthew Aileru <m.a.aileru@gmail.com>
2022-11-10 06:52:32 +0000

    Update ContactLinks.js

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -4,19 +4,13 @@ import { BsLinkedin } from 'react-icons/bs'
 const ContactLinks = () => {
     return (
         <div className="contact-links">
-                {/* <button className="contact-links__email">
-                    <a href="#" className="contact-links__email-link"><MdEmail />Email</a>
-                </button>
-                <button className="contact-links__linkedin">
-                    <a href="#" className="contact-links__linkedin-link"><BsLinkedin />LinkedIn</a>
-                </button> */}
-                <a href="mailto:matthew.a.aileru@gmail.com" className="contact-links__email">
-                    <MdEmail />Email
-                </a>
-                <a href="https://www.linkedin.com/in/matthew-aileru-ba9ba094/" className="contact-links__linkedin">
-                    <BsLinkedin />LinkedIn
-                </a>
-            </div>
+            <a href="mailto:matthew.a.aileru@gmail.com" className="contact-links__email">
+                <MdEmail />Email
+            </a>
+            <a href="https://www.linkedin.com/in/matthew-aileru-ba9ba094/" className="contact-links__linkedin" target="_blank">
+                <BsLinkedin />LinkedIn
+            </a>
+        </div>
     )
 }
 
